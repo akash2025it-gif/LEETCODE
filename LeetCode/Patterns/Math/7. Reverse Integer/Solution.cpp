174cbe4ef0b19bce50a8669a70c9b9cd2a4618e6
Akash G <akash2025it@gmail.com>
2026-09-10 20:34:35 +0530

    Add Medium C++ Solution for 7. Reverse Integer from LeetCode

---
+++ LeetCode/Patterns/Math/7. Reverse Integer/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        long long res=0;
+        while(x!=0)
+        {
+            int digit=x%10;
+            res=res*10+digit;
+            x=x/10;
+        }
+        if(res>2147483647||res<-2147483648)
+        {
+            return 0;
+        }
+        return res;
+    }
+};
