3d05e26d769563c3970791b2a9d60ecb66d633ff
Akash G <akash2025it@gmail.com>
2026-09-10 20:46:21 +0530

    Add Easy C++ Solution for 9. Palindrome Number from LeetCode

---
+++ LeetCode/Patterns/Math/9. Palindrome Number/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev=0;
+        int digit;
+        long long temp=x;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x!=0)
+        {
+             digit=x%10;
+             rev=rev*10+digit;
+             x/=10;
+        }
+        if(temp==rev)
+        {
+            return true;
+        }
+        return false;
+    }
+};
