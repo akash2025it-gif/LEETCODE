c6122eee69b11a2faff084ffa2ca0551a1175112
Akash G <akash2025it@gmail.com>
2026-09-13 19:30:39 +0530

    Add Easy C++ Solution for 136. Single Number from LeetCode

---
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        set<int>s;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(s.find(nums[i])!=s.end())
+            {
+                s.erase(nums[i]);
+            }
+            else
+            {
+                s.insert(nums[i]);
+            }
+        }
+          return *s.begin();
+    }
+};
