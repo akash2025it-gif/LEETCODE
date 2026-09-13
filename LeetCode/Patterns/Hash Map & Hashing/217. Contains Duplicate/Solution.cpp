4ca3a63a489f0fe862088ec3c14d248439d658cf
Akash G <akash2025it@gmail.com>
2026-09-13 16:32:18 +0530

    Add Easy C++ Solution for 217. Contains Duplicate from LeetCode

---
+++ LeetCode/Patterns/Hash Map & Hashing/217. Contains Duplicate/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        int n=nums.size();
+        for(int i=0;i<=n-1;i++)
+        {
+           if(s.find(nums[i])!=s.end())
+           {
+            return true;
+           }
+           s.insert(nums[i]);
+        }
+        return false;
+    }
+};
